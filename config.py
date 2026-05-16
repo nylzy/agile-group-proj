6e4f14796f7c532a60f71f95cb3421fc0317baa0
lukasmrohwer <lukasmrohwer@outlook.com>
2026-05-16 22:27:46 +0800

    Move secret key to environment variables and include set up instructions.

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    if not SECRET_KEY:
+        raise ValueError("No SECRET_KEY found in environment variables")
